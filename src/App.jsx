d0f7a90709ea44513941b23bc41ee9672aaf5224
Frederick <frederickperezdelacruz@outlook.com>
2025-11-23 18:37:16 -0400

    feat: Agregar banner de instalación para iOS/iPhone

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/Home/HomePage';
 import BeautySalonDashboard from './components/admin/BeautySalonDashboard/BeautySalonDashboard';
 import InstallPWA from './components/InstallPWA/InstallPWA';
+import InstallPWAiOS from './components/InstallPWA/InstallPWAiOS';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/admin" element={<BeautySalonDashboard />} />
       </Routes>
       <InstallPWA />
+      <InstallPWAiOS />
     </Router>
   );
 }
