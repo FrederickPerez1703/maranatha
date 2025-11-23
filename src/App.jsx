b07ec93cc0f2d03d3d8537984c76e1467b872cfa
Frederick <frederickperezdelacruz@outlook.com>
2025-11-22 22:00:15 -0400

    fix Landinpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/Home/HomePage';
-import Dashboard from './components/admin/Dashboard/Dashboard';
+import BeautySalonDashboard from './components/admin/BeautySalonDashboard/BeautySalonDashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/admin" element={<Dashboard />} />
+        <Route path="/admin" element={<BeautySalonDashboard />} />
       </Routes>
     </Router>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import BeautySalonDashboard from './components/admin/BeautySalonDashboard/BeautySalonDashboard.jsx'
 import { LanguageProvider } from './context/LanguageContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LanguageProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/admin" element={<BeautySalonDashboard />} />
-        </Routes>
-      </BrowserRouter>
+      <App />
     </LanguageProvider>
   </StrictMode>,
 )
