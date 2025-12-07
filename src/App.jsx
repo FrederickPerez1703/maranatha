ddd24da74fe96eda1cbe2db95197f166667052ef
Frederick <frederickperezdelacruz@outlook.com>
2025-12-06 22:10:19 -0400

    Fix abaut

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/Home/HomePage';
-import AboutUs from './components/Home/AboutUs';
+import About from './components/Home/About';
 import BeautySalonDashboard from './components/admin/BeautySalonDashboard/BeautySalonDashboard';
 import InstallPWA from './components/InstallPWA/InstallPWA';
 import InstallPWAiOS from './components/InstallPWA/InstallPWAiOS';
@@ -14,7 +14,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/about" element={<AboutUs />} />
+            <Route path="/about" element={<About />} />
             <Route path="/admin" element={<BeautySalonDashboard />} />
           </Routes>
           <InstallPWA />
